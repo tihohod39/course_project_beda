5eb666184fa9c822424a3afe31414fece88c2e81
группы стационара <st@itstep.lan>
2026-04-03 19:31:35 +0300

    настройки в основе

--- Java.js
+++ Java.js
@@ -1,3 +1,4 @@
+let theme = 1;
 document.getElementById("btnOpen").onclick = function(){
     let vid = document.getElementsByClassName("levels");
             vid[0].style.visibility = "visible";
@@ -9,4 +10,46 @@ document.getElementById("btnClose").onclick = function(){
             vid[0].style.visibility = "hidden";
             let vid2 = document.getElementsByClassName("buttons");
             vid2[0].style.visibility = "visible";
+}
+document.getElementById("btnOptions").onclick = function(){
+    let vid = document.getElementsByClassName("options");
+    vid[0].style.visibility = "visible";
+}
+document.getElementById("btnClose2").onclick = function(){
+    let vid = document.getElementsByClassName("options");
+    vid[0].style.visibility = "hidden";
+}
+
+document.getElementById("btnTheme").onclick = function(){
+    theme +=1;
+ if(theme>4){
+     theme=1;
+ }
+ switch (theme) {
+    case 1:{
+        let vid = document.getElementsByClassName("all")
+       
+        vid[0].style.background = 'rgb(37, 37, 37)';
+    break;
+    }
+    case 2:{
+        let vid = document.getElementsByClassName("all")
+        let all = document.getElementsByClassName("button")
+        all.forEach(element => {
+           element.style.background ='rgb(143 223 220)'
+        });
+        vid[0].style.background = 'rgb(143 223 220)';
+    break;
+    }   
+    case 3:{
+        let vid = document.getElementsByClassName("all")
+        vid[0].style.background = 'rgb(143 145 223)';
+    break;
+    }
+    case 4:{
+        let vid = document.getElementsByClassName("all")
+        vid[0].style.background = 'rgb(214 223 143)';
+    break;
+    }         
+ };
 }
